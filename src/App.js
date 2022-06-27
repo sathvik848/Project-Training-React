b19ac2ee0336e861c01ba8eb9dccb0b8cf301355
Sathvik <sathvikkrishna12345@gmail.com>
2022-06-27 11:46:07 +0530

    Added my Sixth  project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+import './App.css'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import CounterThree from './components/CounterThree'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
+import DataFetchingOne from './components/DataFetchingOne'
+import DataFetchingTwo from './components/DataFetchingTwo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = 0
+const reducer = (state, action) => {
+	switch (action) {
+		case 'increment':
+			return state + 1
+		case 'decrement':
+			return state - 1
+		case 'reset':
+			return initialState
+		default:
+			return state
+	}
 }
 
-export default App;
+export const CountContext = React.createContext()
+
+function App() {
+	const [count, dispatch] = useReducer(reducer, initialState)
+	return (
+		<CountContext.Provider
+			value={{ countState: count, countDispatch: dispatch }}
+		>
+			<div className="App">
+				{/* <CounterOne /> */}
+				{/* <CounterTwo /> */}
+				{/* <CounterThree /> */}
+				{/* {count} */}
+				{/* <ComponentA /> */}
+				{/* <ComponentB /> */}
+				{/* <ComponentC /> */}
+				{/* <DataFetchingOne /> */}
+				<DataFetchingTwo />
+			</div>
+		</CountContext.Provider>
+	)
+}
+
+export default App
